57cd3c5d13f55c383b981917d904b915c6e9e13e
PRERAN S <prerans181@gmail.com>
2026-07-05 15:26:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-mongoose.connect("mongodb://127.0.0.1:27017/ezyapi");
+mongoose.connect("mongodb+srv://preran248:preran123@cluster0.gqh6dfj.mongodb.net/plusapi?appName=Cluster0");
 
 mongoose.connection.once("open", () => {
     console.log("Mongo Connected");
